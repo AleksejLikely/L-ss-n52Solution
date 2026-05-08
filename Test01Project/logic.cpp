967a508c75038b2083435894e53ec822c02c7270
Aleksej.Cheh <aleksej.cheh0203@gmail.com>
2026-05-08 17:59:54 +0300

    Finish Test01Project

--- Test01Project/logic.cpp
+++ Test01Project/logic.cpp
@@ -1,20 +1,29 @@
 #include "logic.h"
 
 void sort_bubble(int array[], int size) {
+	//int count = 0;
+
 	if (size <= 1) {
 		return;
 	}
 
 	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < (size - 1); j++) {
+		bool flag = true;
+		for (int j = 0; j < (size - 1 - i); j++) {
+			//count++;
 			if (array[j] > array[j + 1]) {
+				flag = false;
 				int t = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = t;
-			}
 
+
+
+			}
+		}
+		if (flag ) {
+			break;
 		}
 	}
-
-
+	//cout << "\nCount = " << count;
 }
