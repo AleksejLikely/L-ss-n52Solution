bcbd999043046f06a5344e8681888a14cdb65383
Aleksej.Cheh <aleksej.cheh0203@gmail.com>
2026-05-08 17:37:35 +0300

    And sort algoritm

--- Test01Project/main.cpp
+++ Test01Project/main.cpp
@@ -1,17 +1,24 @@
 #include "util.h"
+#include "logic.h"
 
 int main() {
 
 	int array[DEFAULT_SIZE];
 	int size;
 
-	cout << "Input the szie of arra: ";
+	cout << "Input the szie of array: ";
 	cin >> size;
 
 	init_random(array, size, -100, 100);
 
-	print("Array before: ");
+	print("Array before: ");	
 	print(convert(array, size));
+
+	sort_bubble(array, size);
+
+	print("\nArray after sorting: ");
+	print(convert(array, size));
+	print("\n");
 
 	return 0;
 
